c70f35ed3e796fc471da0d02a95cb28b7be9516f
oran sk <oransk96@gmail.com>
2026-02-04 10:13:21 +0530

    add user registration route; implement async error handling and update app structure

--- src/index.js
+++ src/index.js
@@ -1,43 +1,15 @@
-// require("dotenv").config({ path: "./env" });
 import dotenv from "dotenv";
+import app from "./app.js";
 import connectDB from "./db/index.js";
 
-dotenv.config({ path: "./env" });
+dotenv.config({ path: ".env" });
 
 connectDB()
   .then(() => {
-    app.on("error", (err) => {
-      console.log("err", err);
-    });
     app.listen(process.env.PORT || 8000, () => {
-      console.log(`server is running at port : ${process.env.PORT}`);
+      console.log(`🚀 Server running on port ${process.env.PORT || 8000}`);
     });
   })
   .catch((error) => {
-    console.error("Error connecting to MongoDB:", error);
+    console.error("Server failed to start ❌", error);
   });
-
-  
-/*
-
-import express from "express";
-const app = express();
-
-(async () => {
-  try {
-    await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
-    app.on("error", (error) => {
-      console.error("MongoDB connection error", error);
-      throw error;
-    });
-
-    app.listen(process.env.PORT, () => {
-      console.log(`Server is running on port ${process.env.PORT}`);
-    });
-
-    console.log("Connected to MongoDB");
-  } catch (error) {
-    console.error("Error connecting to MongoDB:", error);
-  }
-})();
-*/
